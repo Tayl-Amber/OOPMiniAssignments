b6ea8922aefa23d767a68e9944033a8db99bd041
Tayl-Amber <taylamber352@gmail.com>
2024-01-28 21:40:07 +0500

    Object Created

--- Reverse.cpp
+++ Reverse.cpp
@@ -10,20 +10,29 @@ private:
 
 public:
     Reverse(int n = 0) : n{n} {}
-    int calRev(){
-         int reversedNumber = 0;
-    while (n!= 0)
+    void set_data(int n)
     {
-      reversedNumber = reversedNumber * 10 + n % 10;
-      n /= 10;
+        this->n = n;
     }
-    return reversedNumber;
+    int calRev()
+    {
+        int reversedNumber = 0;
+        while (n != 0)
+        {
+            reversedNumber = reversedNumber * 10 + n % 10;
+            n /= 10;
+        }
+        return reversedNumber;
     }
-    void display(){
-       cout << "Reversed Numberis: " << calRev() << endl;
+    void display()
+    {
+        cout << "Reversed Numberis: " << calRev() << endl;
     }
 };
 
 int main()
 {
+    Reverse rev;
+    rev.set_data(3244);
+    rev.display();
 }
