6464d1879ea17c0517dc6716e7b5c45fc60e92b6
Tayl-Amber <taylamber352@gmail.com>
2024-01-31 21:31:40 +0500

    Reverse Fuction Added

--- Reverse.cpp
+++ Reverse.cpp
@@ -1,18 +1,26 @@
-#include<iostream>
-#include<string>
-            
-using namespace std;
-            
-class Reverse{
-private:
-            
-public:
-            
-};
-            
-            
-int main(){
-            
-            
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+class Reverse
+{
+private:
+    int n;
+
+public:
+    Reverse(int n = 0) : n{n} {}
+    int calRev(){
+         int reversedNumber = 0;
+    while (n!= 0)
+    {
+      reversedNumber = reversedNumber * 10 + n % 10;
+      n /= 10;
+    }
+    return reversedNumber;
+    }
+};
+
+int main()
+{
 }
