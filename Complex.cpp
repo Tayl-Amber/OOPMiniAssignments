c8e9f3046697818320680f9323632fef87b46576
Tayl-Amber <taylamber352@gmail.com>
2024-03-01 11:16:27 +0500

    Complex Class Added

---
+++ Complex.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Complex
+{
+private:
+    float real;
+    float img;
+
+public:
+    void set_real(float real)
+    {
+        this->real = real;
+    }
+    void set_img(float img)
+    {
+        this->img = img;
+    }
+    void dispaly()
+    {
+        cout << "The Complex Number is: " << real << " + " << img << "i" << endl;
+    }
+    float sum(Complex c1, Complex c2)
+    {
+        float R = c1.real + c2.real;
+        float I = c1.img + c2.img;
+        cout << "The Sum is: " << R << " + " << I << "i" << endl;
+    }
+};
+
+int main()
+{
+    float img, real;
+    Complex c1, c2, c3;
+    
+    cout << endl;
+    cout << "Enter the Real Part for number 1: ";
+    cin >> real;
+    c1.set_real(real);
+    cout << "Enter the Imaginary Part for number 1: ";
+    cin >> img;
+    c1.set_img(img);
+
+    cout << endl;
+    cout << "Enter the Real Part for number 2: ";
+    cin >> real;
+    c2.set_real(real);
+    cout << "Enter the Imaginary Part for number 2: ";
+    cin >> img;
+    c2.set_img(img);
+
+    cout << endl;
+    c1.dispaly();
+    c2.dispaly();
+
+    cout << endl;
+    c3.sum(c1, c2);
+    cout << endl;
+}
