c5a09a69d77a03aab8c071ccb2072188b2d25828
Tayl-Amber <taylamber352@gmail.com>
2024-03-02 05:40:56 +0500

    Time Class Created

---
+++ Time.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Time
+{
+private:
+    int min;
+    int hour;
+    public:
+    void set_data(int min, int hour){
+        this->min = min;
+        this->hour = hour;
+    }
+    void display(){
+        cout << "Mins are: " << min << endl;
+        cout << "Hours are: " << hour << endl;
+    }
+    
+    Time sum(Time &time){
+        Time result;
+        result.min 
+    }
+};
