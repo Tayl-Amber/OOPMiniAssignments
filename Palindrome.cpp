a6d9e71b6e430da9d659986abac5495fa84cf630
TaylAmber <taylamber352@gmail.com>
2024-03-11 06:18:14 +0500

    Added

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -16,7 +16,7 @@ string str = to_string(n);
     int len = str.length();
 
     for (int i = 0; i < len / 2; i++) {
-        // Compare the characters from the beginning and end of the string
+     
         if (str[i] != str[len - i - 1]) {
             // If characters don't match, the number is not a palindrome
             return false;
