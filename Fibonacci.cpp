45d05ca3471b0d0c00e27732ae96ef155b238d62
Tayl-Amber <taylamber352@gmail.com>
2024-03-12 04:20:10 +0500

    Code Formatted using Scope Resolution Operator

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -9,33 +9,41 @@ private:
     int n;
 
 public:
-  Fibonacci(int n = 0) : n{n}{
-
-  }
-    void set_data(int n)
-    {
-        this->n = n;
-    }
-    int fibo(int n)
-    {
-        if (n <= 1)
-        {
-            return n;
-        }
-        return fibo(n - 1) + fibo(n - 2);
-    }
-    void display()
-    {
-        cout << "Fibonacci Series is: " << endl;
-        for (int i = 0; i<=n ; ++i){
-            cout << fibo(i) << " " ;
-        }
-    }
+    Fibonacci(int n = 0) : n{n} {}
+    void set_data(int n);
+    int fibo(int n);
+    void display();
+    ~Fibonacci();
 };
+
+// FUNCTIONS DEFINING OUTSIDE THE CLASS
+void Fibonacci ::set_data(int n)
+{
+    this->n = n;
+}
+int Fibonacci ::fibo(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    return fibo(n - 1) + fibo(n - 2);
+}
+void Fibonacci ::display()
+{
+    cout << "Fibonacci Series is: " << endl;
+    for (int i = 0; i <= n; ++i)
+    {
+        cout << fibo(i) << " ";
+    }
+}
+Fibonacci ::~Fibonacci()
+{
+}
 
 int main()
 {
-       int n;
+    int n;
     Fibonacci fibonacci(n);
     cout << "Enter the number: ";
     cin >> n;
