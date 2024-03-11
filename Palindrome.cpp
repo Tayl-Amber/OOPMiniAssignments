65f69cd4d5dd1827ef267d7426379ed7f87c1eb4
TaylAmber <taylamber352@gmail.com>
2024-03-11 06:17:23 +0500

    Added

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -12,7 +12,19 @@ void display();
             int t;
 };
          bool Palindrome ::    checkPalindrome(){
+string str = to_string(num);
+    int len = str.length();
 
+    // Iterate over the first half of the string
+    for (int i = 0; i < len / 2; i++) {
+        // Compare the characters from the beginning and end of the string
+        if (str[i] != str[len - i - 1]) {
+            // If characters don't match, the number is not a palindrome
+            return false;
+        }
+    }
+    // If all characters match, the number is a palindrome
+    return true;
          }
             
 int main(){
