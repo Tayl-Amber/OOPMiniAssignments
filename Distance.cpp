fbd90d38b4eb35b65bbc155ef6a67f6409660a9a
Tayl-Amber <taylamber352@gmail.com>
2024-03-12 03:55:13 +0500

    Code formatted using Scope Resolution Operator

--- Distance.cpp
+++ Distance.cpp
@@ -10,26 +10,36 @@ private:
     float inches;
 
 public:
-    Distance() : feet{0}, inches{0.0} {} // Default Constructor
-    Distance(int feet, float inches) : feet{feet}, inches{inches} {} //parametrized constructor
-    void set_data(int feet, float inches)
-    {
-        this->feet = feet;
-        this->inches = inches;
-    }
-    void dispaly()
-    {
-        cout << "Feet are: " << feet << endl;
-        cout << "Inches are: " << inches << endl;
-    }
-    Distance add(Distance d1) // passing object as parameter
-    {
-        Distance res;
-        res.feet = feet + d1.feet;
-        res.inches = inches + d1.inches;
-        return res; // returning object from a function
-    }
+    Distance() : feet{0}, inches{0.0} {}                             // Default Constructor
+    Distance(int feet, float inches) : feet{feet}, inches{inches} {} // parametrized constructor
+    void set_data(int feet, float inches);
+    void display();
+    Distance add(Distance d1); // passing object as parameter
+    ~Distance();
 };
+
+  // FUNCTIONS DEFINATION OUTSIDE THE CLASS
+void Distance ::set_data(int feet, float inches)
+{
+    this->feet = feet;
+    this->inches = inches;
+}
+void Distance ::display()
+{
+    cout << "Feet are: " << feet << endl;
+    cout << "Inches are: " << inches << endl;
+}
+Distance Distance ::add(Distance d1)
+{
+    Distance res;
+    res.feet = feet + d1.feet;
+    res.inches = inches + d1.inches;
+    return res; // returning object from a function
+}
+Distance ::~Distance()
+{
+}
+
 
 int main()
 {
@@ -39,14 +49,14 @@ int main()
 
     cout << endl;
     cout << "=======Distance 1=======" << endl;
-    d1.dispaly();
+    d1.display();
 
     cout << endl;
     cout << "=======Distance 2=======" << endl;
-    d2.dispaly();
+    d2.display();
 
     d3 = d1.add(d2);
     cout << endl;
     cout << "The Sum is: " << endl;
-    d3.dispaly();
+    d3.display();
 }
